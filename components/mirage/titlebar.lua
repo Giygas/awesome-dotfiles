ea96cbd65a70d8b79dbce07ba022519715014475
Giygas <gustavorossich@gmail.com>
2022-07-21 21:56:05 -0300

    Added rounded corners and good Blur

--- components/mirage/titlebar.lua
+++ components/mirage/titlebar.lua
@@ -21,21 +21,21 @@ local dpi = require("beautiful").xresources.apply_dpi
 
 
 -- Add a titlebar
-client.connect_signal("request::titlebars", function(c)
-   local titlebar = awful.titlebar(c, {
-      size = dpi(30)
-   })
+-- client.connect_signal("request::titlebars", function(c)
+--    local titlebar = awful.titlebar(c, {
+--       size = dpi(10)
+--    })
 
-   titlebar: setup {
-      {
-         -- AwesomeWM native buttons (images loaded from theme)
-         wibox.layout.margin(awful.titlebar.widget.closebutton(c), dpi(11), dpi(5), dpi(5), dpi(5)),
-         wibox.layout.margin(awful.titlebar.widget.minimizebutton(c), dpi(4), dpi(5), dpi(5), dpi(5)),
-         wibox.layout.margin(awful.titlebar.widget.maximizedbutton(c), dpi(4), dpi(5), dpi(5), dpi(5)),
-         layout = wibox.layout.fixed.horizontal
-      },
-      nil,
-      nil,
-      layout = wibox.layout.align.horizontal
-   }
-end)
+--    titlebar: setup {
+--       {
+--          -- AwesomeWM native buttons (images loaded from theme)
+--          wibox.layout.margin(awful.titlebar.widget.closebutton(c), dpi(11), dpi(5), dpi(5), dpi(5)),
+--          wibox.layout.margin(awful.titlebar.widget.minimizebutton(c), dpi(4), dpi(5), dpi(5), dpi(5)),
+--          wibox.layout.margin(awful.titlebar.widget.maximizedbutton(c), dpi(4), dpi(5), dpi(5), dpi(5)),
+--          layout = wibox.layout.fixed.horizontal
+--       },
+--       nil,
+--       nil,
+--       layout = wibox.layout.align.horizontal
+--    }
+-- end)
