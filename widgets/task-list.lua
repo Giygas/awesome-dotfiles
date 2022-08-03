baebe2366b521fe8430aff4740b0c84c99ef7742
Giygas <gustavorossich@gmail.com>
2022-08-02 21:54:29 -0300

    Circled background

--- widgets/task-list.lua
+++ widgets/task-list.lua
@@ -55,109 +55,125 @@ local function create_buttons(buttons, object)
 end
 
 
+-- local function list_update(w, buttons, label, data, objects)
+--    -- update the widgets, creating them if needed
+--    w:reset()
+--    for i, o in ipairs(objects) do
+--       local cache = data[o]
+--       local ib, cb, tb, cbm, bgb, tbm, ibm, tt, l, ll, bg_clickable
+--       if cache then
+--          ib = cache.ib
+--          tb = cache.tb
+--          bgb = cache.bgb
+--          tbm = cache.tbm
+--          ibm = cache.ibm
+--          tt  = cache.tt
+--       else
+--          ib = wibox.widget.imagebox()
+--          tb = wibox.widget.textbox()
+--          -- cb = clickable_container(wibox.container.margin(wibox.widget.imagebox(ICON_DIR .. "close.svg"), dpi(6), dpi(6), dpi(6), dpi(6)))
+--          -- cb.shape = gears.shape.circle
+--          cbm = wibox.container.margin(cb, dpi(4), dpi(8), dpi(2), dpi(2)) -- 4, 8 ,12 ,12 -- close button
+--          -- cbm:buttons(gears.table.join(awful.button({}, 1, nil,
+--          --    function()
+--          --       o.kill(o)
+--          --    end
+--          -- )))
+--          bg_clickable = clickable_container()
+--          bgb = wibox.container.background()
+--          tbm = wibox.container.margin(tb, dpi(4), dpi(4))
+--          ibm = wibox.container.margin(ib, dpi(6), dpi(6), dpi(6), dpi(6)) -- 12 is default top and bottom margin --app icon
+--          l = wibox.layout.fixed.horizontal()
+--          ll = wibox.layout.fixed.horizontal()
+
+--          -- All of this is added in a fixed widget
+--          l:fill_space(true)
+--          l:add(ibm)
+--          l:add(tbm)
+--          ll:add(l)
+--          ll:add(cbm)
+
+--          bg_clickable:set_widget(ll)
+--          -- And all of this gets a background
+--          bgb:set_widget(bg_clickable)
+
+--          l:buttons(create_buttons(buttons, o))
+
+--          -- Tooltip to display whole title, if it was truncated
+--          tt = awful.tooltip({
+--             objects = {tb},
+--             mode = 'outside',
+--             align = 'bottom',
+--             delay_show = 1,
+--          })
+
+--          data[o] = {
+--             ib = ib,
+--             tb = tb,
+--             bgb = bgb,
+--             tbm = tbm,
+--             ibm = ibm,
+--             tt  = tt
+--          }
+--       end
+
+--       local text, bg, bg_image, icon, args = label(o, tb)
+--       args = args or {}
+
+--       -- The text might be invalid, so use pcall.
+--       if text == nil or text == '' then
+--          tbm:set_margins(0)
+--       else
+--           -- truncate when title is too long
+--          local text_only = text:match('>(.-)<')
+--          if (text_only:len() > 8) then
+--             text = text:gsub('>(.-)<', '>' .. text_only:sub(1, 8) .. '...<')
+--             tt:set_text(text_only)
+--             tt:add_to_object(tb)
+--          else
+--             tt:remove_from_object(tb)
+--          end
+--          if not tb:set_markup_silently(text) then
+--             tb:set_markup('<i>&lt;Invalid text&gt;</i>')
+--          end
+--       end
+--       bgb:set_bg(bg)
+--       if type(bg_image) == 'function' then
+--          -- TODO: Why does this pass nil as an argument?
+--          bg_image = bg_image(tb, o, nil, objects, i)
+--       end
+--       bgb:set_bgimage(bg_image)
+--          if icon then
+--             ib.image = icon
+--          else
+--             ibm:set_margins(0)
+--          end
+
+--       bgb.shape = gears.shape.rounded_bar
+--       -- -- bgb.shape_border_width = 1
+--       -- bgb.shape_border_color = '#00000022'
+--       -- bgb:set_bg("#FFFFFF10")
+      
+--       w:add(bgb)
+--    end
+-- end
+
 local function list_update(w, buttons, label, data, objects)
    -- update the widgets, creating them if needed
    w:reset()
+   local dot
    for i, o in ipairs(objects) do
-      local cache = data[o]
-      local ib, cb, tb, cbm, bgb, tbm, ibm, tt, l, ll, bg_clickable
-      if cache then
-         ib = cache.ib
-         tb = cache.tb
-         bgb = cache.bgb
-         tbm = cache.tbm
-         ibm = cache.ibm
-         tt  = cache.tt
-      else
-         ib = wibox.widget.imagebox()
-         tb = wibox.widget.textbox()
-         -- cb = clickable_container(wibox.container.margin(wibox.widget.imagebox(ICON_DIR .. "close.svg"), dpi(6), dpi(6), dpi(6), dpi(6)))
-         -- cb.shape = gears.shape.circle
-         cbm = wibox.container.margin(cb, dpi(4), dpi(8), dpi(2), dpi(2)) -- 4, 8 ,12 ,12 -- close button
-         -- cbm:buttons(gears.table.join(awful.button({}, 1, nil,
-         --    function()
-         --       o.kill(o)
-         --    end
-         -- )))
-         bg_clickable = clickable_container()
-         bgb = wibox.container.background()
-         tbm = wibox.container.margin(tb, dpi(4), dpi(4))
-         ibm = wibox.container.margin(ib, dpi(6), dpi(6), dpi(6), dpi(6)) -- 12 is default top and bottom margin --app icon
-         l = wibox.layout.fixed.horizontal()
-         ll = wibox.layout.fixed.horizontal()
-
-         -- All of this is added in a fixed widget
-         l:fill_space(true)
-         l:add(ibm)
-         l:add(tbm)
-         ll:add(l)
-         -- ll:add(cbm)
-
-         bg_clickable:set_widget(ll)
-         -- And all of this gets a background
-         bgb:set_widget(bg_clickable)
-
-         l:buttons(create_buttons(buttons, o))
-
-         -- Tooltip to display whole title, if it was truncated
-         tt = awful.tooltip({
-            objects = {tb},
-            mode = 'outside',
-            align = 'bottom',
-            delay_show = 1,
-         })
-
-         data[o] = {
-            ib = ib,
-            tb = tb,
-            bgb = bgb,
-            tbm = tbm,
-            ibm = ibm,
-            tt  = tt
-         }
-      end
-
-      local text, bg, bg_image, icon, args = label(o, tb)
-      args = args or {}
-
-      -- The text might be invalid, so use pcall.
-      if text == nil or text == '' then
-         tbm:set_margins(0)
-      else
-          -- truncate when title is too long
-         local text_only = text:match('>(.-)<')
-         if (text_only:len() > 8) then
-            text = text:gsub('>(.-)<', '>' .. text_only:sub(1, 8) .. '...<')
-            tt:set_text(text_only)
-            tt:add_to_object(tb)
-         else
-            tt:remove_from_object(tb)
-         end
-         if not tb:set_markup_silently(text) then
-            tb:set_markup('<i>&lt;Invalid text&gt;</i>')
-         end
-      end
-      bgb:set_bg(bg)
-      if type(bg_image) == 'function' then
-         -- TODO: Why does this pass nil as an argument?
-         bg_image = bg_image(tb, o, nil, objects, i)
-      end
-      bgb:set_bgimage(bg_image)
-         if icon then
-            ib.image = icon
-         else
-            ibm:set_margins(0)
-         end
-
-      bgb.shape = gears.shape.rounded_bar
-      -- -- bgb.shape_border_width = 1
-      -- bgb.shape_border_color = '#00000022'
-      -- bgb:set_bg("#FFFFFF10")
-      
-      w:add(bgb)
+      dot = wibox.container.background()
+      dot.bg = '#000000'
+      dot.shape = gears.shape.rounded_bar
+      dot.shape_border_width = 2
+      dot.shape_border_color = "#AAAAAA"
+      dot.forced_height = 0
+      dot.forced_width = 40
+      cdot = wibox.container.margin(dot, dpi(10), dpi(20), dpi(30), dpi(20))
+      w:add(dot)
    end
 end
-
 
 -- ===================================================================
 -- Widget Creation
@@ -211,6 +227,7 @@ task_list.create = function(s)
          spacing_widget = {
             forced_width = 5,
             shape = gears.shape.circle,
+            bg = '#000000',
             widget = wibox.widget.separator,
          },
          
